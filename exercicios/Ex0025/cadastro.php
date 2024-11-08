c924b011841ef36afa9fd18d277458123d6d684b
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2024-11-08 15:33:24 -0300

    formularios completos (por enquanto)

--- exercicios/Ex0025/cadastro.php
+++ exercicios/Ex0025/cadastro.php
@@ -1,2 +1,7 @@
 Os dados foram enviados!
 Aprenda PHP para saber o que fazer com eles.
+<form action="cadastro.php">
+
+<input type="submit" value="Enviar">
+<input type="reset" value="Limpar">
+    </form>
